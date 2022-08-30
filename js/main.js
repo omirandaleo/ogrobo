a32eb98525a62d68e6a93b88fd3b8af679c1542a
gabrielfins <contato.gabrielfins@gmail.com>
2022-08-30 01:03:04 -0300

    pronto

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+let hasClicked = false;
+
 document.addEventListener('DOMContentLoaded', () => {
   const easterEgg = document.querySelector('.easter-egg');
   setInterval(() => {
+    if (!hasClicked) return;
     easterEgg.classList.remove('closed');
     const audio = new Audio('https://github.com/omirandaleo/ogrobo/blob/main/audio/toasty.mp3?raw=true');
     audio.play();
@@ -9,3 +12,5 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 1300)
   }, 20000)
 });
+
+document.addEventListener('click', () => hasClicked = true);
