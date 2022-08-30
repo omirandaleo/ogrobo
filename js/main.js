00d31604e507432ee2dbbcc95e60b01b10e0e7b5
gabrielfins <contato.gabrielfins@gmail.com>
2022-08-30 00:41:03 -0300

    toasty 3

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const easterEgg = document.querySelector('.easter-egg');
   setInterval(() => {
     easterEgg.classList.remove('closed');
-    const audio = new Audio('../audio/toasty.mp3');
+    const audio = new Audio('https://github.com/omirandaleo/ogrobo/blob/main/audio/toasty.mp3?raw=true');
     audio.play();
     setTimeout(() => {
       easterEgg.classList.add('closed');
